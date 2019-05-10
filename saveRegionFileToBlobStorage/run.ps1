c004fa22006554e4656cd0d25ee035fff8250d3b
Kieran Jacobsen <code@poshsecurity.com>
2019-05-10 16:44:58 +1000

    Code to save xml files to blob storage

---
+++ saveRegionFileToBlobStorage/run.ps1
@@ -0,0 +1,29 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+try {
+    $baseURL = 'https://azureregions.azurewebsites.net/api/GetRegionFile?Code={0}&Region={1}'
+    $authCode = $ENV:AdminAuth
+    $regionFiles = @('Standard', 'China', 'Germany')
+    foreach ($regionFile in $regionFiles) {
+        $fileURL = $baseURL -f $authCode, $regionFile
+        $apiResponse = invoke-webrequest $fileURL -UseBasicParsing
+        Push-OutputBinding -Name $regionFile -Value $apiResponse
+    }
+    $status = [HttpStatusCode]::OK
+    $Body = 'success'
+} catch {
+    $status = [HttpStatusCode]::BadRequest
+    $Body = 'failed'
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = $status
+    Body = $body
+})
